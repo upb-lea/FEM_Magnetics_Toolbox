fa2bc2215a858d7142e3c10c4cf2c361b826ea91
Nikolas Förster <foerster@lea.uni-paderborn.de>
2022-02-15 19:20:18 +0100

    sphinx multiversion fix

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 import time
-sys.path.insert(0, os.path.abspath('../femmt'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
