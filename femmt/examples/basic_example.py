069b566267b74ebffdaddbbef126585921b2a044
Jonas Hoelscher <jonasho@campus.uni-paderborn.de>
2022-02-16 13:18:36 +0100

    New basic example

--- femmt/examples/basic_example.py
+++ femmt/examples/basic_example.py
@@ -3,7 +3,7 @@ from femmt import *
 import numpy as np
 
 # component = "inductor"
-component = "transformer"
+component = "transformer interleaved"
 # component = "integrated_transformer"
 
 
@@ -25,9 +25,9 @@ if component == "inductor":
     # geo.single_simulation(freq=100000, current=[1])
     geo.femm_reference(freq=100000, current=[1], sigma_cu=58, sign=[1], non_visualize=0)
 
-if component == "transformer":
+if component == "transformer-interleaved":
     geo = MagneticComponent(component_type="transformer")
-    geo.visualize_before = True
+    geo.visualize_before = False
 
     # Update Geometry
     geo.core.update(window_h=0.0295, window_w=0.012, core_w=0.015)
@@ -36,11 +36,11 @@ if component == "transformer":
     geo.air_gaps.update(method="percent", n_air_gaps=1, air_gap_h=[0.0005],
                         air_gap_position=[50], position_tag=[0])
 
-    geo.update_conductors(n_turns=[[10, 0], [0, 10]], conductor_type=["solid", "litz"],
+    geo.update_conductors(n_turns=[[36], [11]], conductor_type=["solid", "litz"],
                         litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
                         ff=[None, 0.6], strands_numbers=[None, 600], strand_radii=[70e-6, 35.5e-6],
                         conductor_radii=[0.0011, None],
-                        winding=["primary", "secondary"], scheme=["square", "square"],
+                        winding=["interleaved"], scheme=["horizontal"],
                         core_cond_isolation=[0.0005, 0.0005], cond_cond_isolation=[0.0002, 0.0002, 0.0005])
 
     # Perform a single simulation
@@ -101,6 +101,29 @@ if component == "transformer":
 
     geo.thermal_simulation(thermal_conductivity_dict, boundary_temperatures, boundary_flags, case_gap_top, case_gap_right, case_gap_bot)
     geo.femm_thermal_validation(thermal_conductivity_dict, femm_boundary_temperature)
+
+if component == "transformer":
+    # Example for a transformer with multiple virtual winding windows.
+    geo = MagneticComponent(component_type="transformer")
+    geo.visualize_before = True
+
+    # Update Geometry
+    geo.core.update(window_h=0.0295, window_w=0.012, core_w=0.015)
+
+    # geo.air_gaps.update(n_air_gaps=0)
+    geo.air_gaps.update(method="percent", n_air_gaps=1, air_gap_h=[0.0005],
+                        air_gap_position=[50], position_tag=[0])
+
+    geo.update_conductors(n_turns=[[10, 0], [0, 10]], conductor_type=["solid", "litz"],
+                        litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
+                        ff=[None, 0.6], strands_numbers=[None, 600], strand_radii=[70e-6, 35.5e-6],
+                        conductor_radii=[0.0011, None],
+                        winding=["primary", "secondary"], scheme=["square", "square"],
+                        core_cond_isolation=[0.0005, 0.0005], cond_cond_isolation=[0.0002, 0.0002, 0.0005])
+
+
+    geo.single_simulation(freq=250000, current=[4.14723021, 14.58960019], phi_deg=[- 1.66257715/np.pi*180, 170])
+
 
 if component == "integrated_transformer":
     geo = MagneticComponent(component_type="integrated_transformer")
