d2562248fa1ced56a4a2ce11d51ef8d997bfcb9f
Jonas Hoelscher <jonasho@campus.uni-paderborn.de>
2022-02-16 17:14:51 +0100

    First version of integration test.

--- tests/integration/test_femmt.py
+++ tests/integration/test_femmt.py
@@ -1,12 +1,29 @@
 import pytest
 import numpy as np
+import os
+import shutil
 from femmt import MagneticComponent
 
-@pytest.mark.integtest
+def compare_mesh(first_mesh, second_mesh):
+    first_content = None
+    second_content = None
+
+    with open(first_mesh, "rb") as fd:
+        first_content = fd.read(-1)
+
+    with open(second_mesh, "rb") as fd:
+        second_content = fd.read(-1)
+
+    return first_content == second_content
+
 def test_femmt():
 
+    # Create new temp folder
+    temp_folder_path = os.path.join(os.path.dirname(__file__), "temp")
+    os.mkdir(temp_folder_path)
+
     # Build model and create test files
-    geo = MagneticComponent(component_type="transformer")
+    geo = MagneticComponent(component_type="transformer", working_directory=temp_folder_path)
     geo.visualize_before = False
     geo.core.update(window_h=0.0295, window_w=0.012, core_w=0.015)
     geo.air_gaps.update(method="percent", n_air_gaps=1, air_gap_h=[0.0005],
@@ -53,3 +70,19 @@ def test_femmt():
     }
 
     geo.thermal_simulation(thermal_conductivity_dict, boundary_temperatures, boundary_flags, case_gap_top, case_gap_right, case_gap_bot)
+
+    # Compare with existing mesh
+    fixture_mesh_folder_path = os.path.join(os.path.dirname(__file__), "fixtures", "mesh")
+
+    # e_m mesh
+    test_e_m_mesh_file = os.path.join(temp_folder_path, "mesh", "electro_magnetic.msh")
+    fixture_e_m_mesh_file = os.path.join(fixture_mesh_folder_path, "electro_magnetic.msh")
+    assert compare_mesh(test_e_m_mesh_file, fixture_e_m_mesh_file), "Electro magnetic mesh is wrong."
+
+    # thermal mesh
+    test_thermal_mesh_file = os.path.join(temp_folder_path, "mesh", "thermal.msh")
+    fixture_thermal_mesh_file = os.path.join(fixture_mesh_folder_path, "thermal.msh")
+    assert compare_mesh(test_thermal_mesh_file, fixture_thermal_mesh_file), "Thermal mesh is wrong."
+
+    # Remove temp files:
+    shutil.rmtree(temp_folder_path)
