f5cf181b83ffc5576ba676eddbdb5f56333770a9
Jonas Hoelscher <jonasho@campus.uni-paderborn.de>
2022-03-21 11:09:46 +0100

    Extracted example thermal simulation

--- femmt/examples/basic_example.py
+++ femmt/examples/basic_example.py
@@ -2,77 +2,7 @@
 from femmt import *
 import numpy as np
 
-# component = "inductor"
-# component = "transformer-interleaved"
-component = "integrated_transformer"
-# component = "transformer"
-
-# Create Object
-if component == "inductor":
-    geo = MagneticComponent(component_type="inductor")
-
-    # Update Geometry
-    geo.core.update(window_h=0.03, window_w=0.011,
-                    mu_rel=3100, phi_mu_deg=12,
-                    sigma=0.6)
-
-    # geo.air_gaps.update(method="percent", n_air_gaps=4, air_gap_h=[0.0005, 0.0005, 0.0005, 0.0005],
-    #                     position_tag=[0, 0, 0, 0], air_gap_position=[20, 40, 60, 80])
-    geo.air_gaps.update(method="center", n_air_gaps=1, air_gap_h=[0.0005], position_tag=[0])
-
-    geo.update_conductors(n_turns=[[14]], conductor_type=["solid"], conductor_radii=[0.0015],
-                          winding=["primary"], scheme=["square"],
-                          core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0001],
-                          conductivity_sigma=["copper"])
-
-    geo.create_model(freq=100000, visualize_before=False, do_meshing=True, save_png=False)
-
-    geo.single_simulation(freq=100000, current=[3], show_results=False)
-
-
-    # Excitation Sweep Example
-    # fs = [0, 10000, 30000, 60000, 100000, 150000]
-    # amplitude_list = [[10], [2], [1], [0.5], [0.2], [0.1]]
-    # phase_list = [[0], [10], [20], [30], [40], [50]]
-    # geo.excitation_sweep(frequency_list=fs, current_list_list=amplitude_list, phi_deg_list_list=phase_list)
-
-    # Reference simulation using FEMM
-    # geo.femm_reference(freq=100000, current=[1], sigma_cu=58, sign=[1], non_visualize=0)
-
-if component == "transformer-interleaved":
-    geo = MagneticComponent(component_type="transformer")
-
-    # Update Geometry
-    geo.core.update(window_h=0.0295, window_w=0.012, core_w=0.015,
-                    non_linear=False, sigma=1, re_mu_rel=3200, phi_mu_deg=10)
-
-    # geo.air_gaps.update(n_air_gaps=0)
-    geo.air_gaps.update(method="percent", n_air_gaps=1, air_gap_h=[0.0005],
-                        air_gap_position=[50], position_tag=[0])
-
-    geo.update_conductors(n_turns=[[21], [7]], conductor_type=["solid", "solid"],
-                        litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
-                        ff=[None, 0.6], strands_numbers=[None, 600], strand_radii=[70e-6, 35.5e-6],
-                        conductor_radii=[0.0011, 0.0011],
-                        winding=["interleaved"], scheme=["horizontal"],
-                        core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0002, 0.0002, 0.0005],
-                        conductivity_sigma=["copper", "copper"])
-
-    # geo.update_conductors(n_turns=[[21], [7]], conductor_type=["litz", "litz"],
-    #                     litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
-    #                     ff=[0.6, 0.6], strands_numbers=[600, 600], strand_radii=[35.5e-6, 35.5e-6],
-    #                     conductor_radii=[0.0011, 0.0011],
-    #                     winding=["interleaved"], scheme=["horizontal"],
-    #                     core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0002, 0.0002, 0.0005],
-    #                     conductivity_sigma=["copper", "copper"])
-
-    # Perform a single simulation
-    geo.create_model(freq=250000, visualize_before=True)
-    geo.single_simulation(freq=250000, current=[4, 12], phi_deg=[0, 180], show_results=True)
-    # geo.get_inductances(I0=8, op_frequency=250000, skin_mesh_factor=0.5)
-    geo.femm_reference(freq=250000, current=[4, 12], sign=[1, -1], non_visualize=0)
-
-    # ----------------------------------------------------------------------------------
+def example_thermal_simulation():
     # Thermal simulation:
     # The losses calculated by the magnetics simulation can be used to calculate the heat distribution of the given magnetic component
     # In order to use the thermal simulation, thermal conductivities for each material can be entered as well as a boundary temperature
@@ -132,8 +62,82 @@ if component == "transformer-interleaved":
     # When the losses file is already created and contains the losses for the current model, it is enough to run geo.create_model in
     # order for the thermal simulation to work (geo.single_simulation is not needed).
     # Obviously when the model is modified and the losses can be out of date and therefore the geo.single_simulation needs to run again.
-    # geo.thermal_simulation(thermal_conductivity_dict, boundary_temperatures, boundary_flags, case_gap_top, case_gap_right, case_gap_bot, True)
+    geo.thermal_simulation(thermal_conductivity_dict, boundary_temperatures, boundary_flags, case_gap_top, case_gap_right, case_gap_bot, True)
     #geo.femm_thermal_validation(thermal_conductivity_dict, femm_boundary_temperature)
+
+component = "inductor"
+# component = "transformer-interleaved"
+#component = "integrated_transformer"
+# component = "transformer"
+
+# Create Object
+if component == "inductor":
+    geo = MagneticComponent(component_type="inductor")
+
+    # Update Geometry
+    geo.core.update(window_h=0.02, window_w=0.0075,
+                    mu_rel=3100, phi_mu_deg=12,
+                    sigma=0.6)
+
+    # geo.air_gaps.update(method="percent", n_air_gaps=4, air_gap_h=[0.0005, 0.0005, 0.0005, 0.0005],
+    #                     position_tag=[0, 0, 0, 0], air_gap_position=[20, 40, 60, 80])
+    geo.air_gaps.update(method="center", n_air_gaps=1, air_gap_h=[0.0005], position_tag=[0])
+
+    geo.update_conductors(n_turns=[[4]], conductor_type=["solid"], conductor_radii=[0.0015],
+                          winding=["primary"], scheme=["square"],
+                          core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0001],
+                          conductivity_sigma=["copper"])
+
+    geo.create_model(freq=100000, visualize_before=True, do_meshing=True, save_png=False)
+
+    geo.single_simulation(freq=100000, current=[3], show_results=False)
+
+    example_thermal_simulation()
+
+    # Excitation Sweep Example
+    # fs = [0, 10000, 30000, 60000, 100000, 150000]
+    # amplitude_list = [[10], [2], [1], [0.5], [0.2], [0.1]]
+    # phase_list = [[0], [10], [20], [30], [40], [50]]
+    # geo.excitation_sweep(frequency_list=fs, current_list_list=amplitude_list, phi_deg_list_list=phase_list)
+
+    # Reference simulation using FEMM
+    # geo.femm_reference(freq=100000, current=[1], sigma_cu=58, sign=[1], non_visualize=0)
+
+if component == "transformer-interleaved":
+    geo = MagneticComponent(component_type="transformer")
+
+    # Update Geometry
+    geo.core.update(window_h=0.0295, window_w=0.012, core_w=0.015,
+                    non_linear=False, sigma=1, re_mu_rel=3200, phi_mu_deg=10)
+
+    # geo.air_gaps.update(n_air_gaps=0)
+    geo.air_gaps.update(method="percent", n_air_gaps=1, air_gap_h=[0.0005],
+                        air_gap_position=[50], position_tag=[0])
+
+    geo.update_conductors(n_turns=[[21], [7]], conductor_type=["solid", "solid"],
+                        litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
+                        ff=[None, 0.6], strands_numbers=[None, 600], strand_radii=[70e-6, 35.5e-6],
+                        conductor_radii=[0.0011, 0.0011],
+                        winding=["interleaved"], scheme=["horizontal"],
+                        core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0002, 0.0002, 0.0005],
+                        conductivity_sigma=["copper", "copper"])
+
+    # geo.update_conductors(n_turns=[[21], [7]], conductor_type=["litz", "litz"],
+    #                     litz_para_type=['implicit_litz_radius', 'implicit_litz_radius'],
+    #                     ff=[0.6, 0.6], strands_numbers=[600, 600], strand_radii=[35.5e-6, 35.5e-6],
+    #                     conductor_radii=[0.0011, 0.0011],
+    #                     winding=["interleaved"], scheme=["horizontal"],
+    #                     core_cond_isolation=[0.001, 0.001, 0.002, 0.001], cond_cond_isolation=[0.0002, 0.0002, 0.0005],
+    #                     conductivity_sigma=["copper", "copper"])
+
+    # Perform a single simulation
+    geo.create_model(freq=250000, visualize_before=True)
+    geo.single_simulation(freq=250000, current=[4, 12], phi_deg=[0, 180], show_results=True)
+    # geo.get_inductances(I0=8, op_frequency=250000, skin_mesh_factor=0.5)
+    geo.femm_reference(freq=250000, current=[4, 12], sign=[1, -1], non_visualize=0)
+
+    example_thermal_simulation()
+    
 
 if component == "transformer":
     # Example for a transformer with multiple virtual winding windows.
