updated logging files (within some sign changes) and bug-fix in femmt_model_classes (implicite litz definition)

femmt/examples/basic_example.py
@@ -73,7 +73,7 @@ def example_thermal_simulation():
 component = "inductor"
 # component = "transformer-interleaved"
 # component = "transformer"
-#component = "integrated_transformer"
+# component = "integrated_transformer"
 
 # Create Object
 if component == "inductor":
@@ -81,14 +81,14 @@ if component == "inductor":
     working_directory = os.path.join(__file__, "..")
 
     # 1. chose simulation type
-    geo = fmt.MagneticComponent(component_type=fmt.ComponentType.Inductor, working_directory=None)
+    geo = fmt.MagneticComponent(component_type=fmt.ComponentType.Inductor, working_directory=working_directory)
 
     # 2. set core parameters
     core_db = fmt.core_database()["PQ 40/40"]
 
     core = fmt.Core(core_w=core_db["core_w"], window_w=core_db["window_w"], window_h=core_db["window_h"],
                     mu_rel=3100, phi_mu_deg=12,
-                    sigma=0.6)
+                    sigma=0.5)
     geo.set_core(core)
 
     # 3. set air gap parameters
@@ -97,8 +97,9 @@ if component == "inductor":
     geo.set_air_gaps(air_gaps)
 
     # 4. set conductor parameters: use solid wires
-    winding = fmt.Winding(8, 0, fmt.Conductivity.Copper, fmt.WindingType.Primary, fmt.WindingScheme.Square)
-    winding.set_solid_conductor(0.0015)
+    winding = fmt.Winding(16, 0, fmt.Conductivity.Copper, fmt.WindingType.Primary, fmt.WindingScheme.Square)
+    # winding.set_solid_conductor(0.0015)
+    winding.set_litz_conductor(conductor_radius=0.0015, number_strands=150, strand_radius=100e-6, fill_factor=None)
     geo.set_windings([winding])
 
     # 5. set isolations
@@ -108,10 +109,10 @@ if component == "inductor":
     geo.set_isolation(isolation)
 
     # 5. create the model
-    # geo.create_model(freq=100000, visualize_before=True, save_png=False)
+    geo.create_model(freq=100000, visualize_before=False, save_png=False)
 
     # 6. start simulation
-    # geo.single_simulation(freq=100000, current=[3], show_results=True)
+    geo.single_simulation(freq=100000, current=[2], show_results=True)
 
     # 7. prepare and start thermal simulation
     #example_thermal_simulation()
@@ -124,7 +125,7 @@ if component == "inductor":
     # geo.excitation_sweep(frequency_list=fs, current_list_list=amplitude_list, phi_deg_list_list=phase_list)
 
     # Reference simulation using FEMM
-    geo.femm_reference(freq=100000, current=[3], sign=[1], non_visualize=0)
+    geo.femm_reference(freq=100000, current=[2], sign=[1], non_visualize=0)
 
 if component == "transformer-interleaved":
     working_directory = os.path.join(__file__, "..")
@@ -183,7 +184,7 @@ if component == "transformer":
     # 2. set core parameters
     core = fmt.Core(window_h=0.0295, window_w=0.012, core_w=0.015,
                     mu_rel=3100, phi_mu_deg=12,
-                    sigma=0.6)
+                    sigma=1.2)
     geo.set_core(core)
 
     # 3. set air gap parameters
@@ -196,7 +197,8 @@ if component == "transformer":
     winding1.set_solid_conductor(0.0011)
 
     winding2 = fmt.Winding(0, 10, fmt.Conductivity.Copper, fmt.WindingType.Secondary, fmt.WindingScheme.Square)
-    winding2.set_litz_conductor(None, 600, 35.5e-6, 0.6)
+    # winding2.set_litz_conductor(None, 600, 35.5e-6, 0.6)
+    winding2.set_solid_conductor(0.0011)
 
     geo.set_windings([winding1, winding2])
 
@@ -208,7 +210,10 @@ if component == "transformer":
 
     # 6. start simulation with given frequency, currents and phases
     geo.create_model(freq=250000, visualize_before=True)
-    geo.single_simulation(freq=250000, current=[4.14723021, 14.58960019], phi_deg=[- 1.66257715/np.pi*180, 170])
+    geo.single_simulation(freq=250000, current=[4, 4], phi_deg=[0, 180])
+
+    # Reference simulation using FEMM
+    geo.femm_reference(freq=250000, current=[4, 4], sign=[-1, 1], non_visualize=0)
 
 if component == "integrated_transformer":
     working_directory = os.path.join(__file__, "..")