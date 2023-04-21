3e498a56fb05eb7e6313dc5dde2ea3e76faf1c70
Nikolas Förster <foerster@lea.uni-paderborn.de>
2023-04-21 16:14:00 +0200

    fix some linting issues

--- femmt/optimization/__init__.py
+++ femmt/optimization/__init__.py
@@ -1,6 +1,5 @@
-from femmt.optimization.ito_brute_force import *
+from femmt.optimization.ito import *
 from femmt.optimization.functions_optimization import *
 from femmt.optimization.integrated_transformer_dtos import *
-from femmt.optimization.ito_optuna import *
 from femmt.optimization.optuna_femmt_parser import *
 from femmt.optimization.ito_functions import *
