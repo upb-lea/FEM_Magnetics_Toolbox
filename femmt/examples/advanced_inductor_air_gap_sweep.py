474e2563ed1e298c11ec7fd7a141a66632b85a7a
abujazar <uthmnn@yahoo.com>
2024-08-07 14:27:02 +0200

    adapting the advanced_inductor_airgap_sweep

--- femmt/examples/advanced_inductor_air_gap_sweep.py
+++ femmt/examples/advanced_inductor_air_gap_sweep.py
@@ -11,7 +11,8 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), "sweep_examples"))
 def basic_example_sweep(onelab_folder: Optional[str] = None, show_visual_outputs: bool = True, is_test: bool = False):
     """Advanced example to demonstrate an air gap sweep for an inductor."""
     # In this sweep an inductor with variable air gap height is simulated
-    air_gap_heights = [0.0020, 0.0010, 0.0005, 0.000025, 0.0000125]
+    #  air_gap_heights = [0.0020, 0.0010, 0.0005, 0.000025, 0.0000125]
+    air_gap_heights = [0.0040, 0.0030, 0.0020, 0.0010, 0.0005]
 
     working_directories = []
 
