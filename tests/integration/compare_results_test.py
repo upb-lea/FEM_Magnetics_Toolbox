74c21c3f39e40c22136a7c4b7241030579c52967
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-08-25 16:32:42 +0200

    split up integration tests in two files

--- tests/integration/compare_results_test.py
+++ tests/integration/compare_results_test.py
@@ -1,35 +1,40 @@
 """Contains integration tests to benchmark new changes against previous simulation results."""
-import pytest
-import os
-import numpy as np
-import json
-import femmt as fmt
-import deepdiff
-import femmt.examples.basic_inductor
-import femmt.examples.basic_transformer_interleaved
-import femmt.examples.basic_transformer
-import femmt.examples.basic_transformer_three_winding
-import femmt.examples.basic_transformer_integrated
-import femmt.examples.basic_transformer_center_tapped
-import femmt.examples.basic_transformer_stacked
-import femmt.examples.basic_transformer_stacked_center_tapped
-import femmt.examples.basic_inductor_foil_vertical
-import femmt.examples.basic_inductor_foil
-import femmt.examples.basic_transformer_n_winding
-import femmt.examples.advanced_inductor_sweep
-import femmt.examples.basic_transformer_5_windings
-import femmt.examples.basic_transformer_6_windings
-import femmt.examples.experimental_inductor_time_domain
-import femmt.examples.experimental_transformer_time_domain
-import femmt.examples.experimental_transformer_three_winding_time_domain
-import femmt.examples.basic_inductor_electrostatic
-import femmt.examples.basic_transformer_electrostatic
-import femmt.examples.advanced_inductor_air_gap_sweep
-import femmt.examples.component_study.transformer_component_study
-import femmt.examples.basic_transformer_excitation_sweep
-import femmt.examples.basic_inductor_excitation_sweep
-import femmt.examples.basic_split_windings
 
+# python libraries
+import json
+import os
+
+# 3rd party libraries
+import deepdiff
+import pytest
+import numpy as np
+
+# own libraries
+import femmt as fmt
+
+@pytest.fixture
+def temp_folder():
+    """Fixture to create the temporary folder the results are stored."""
+    # Setup temp folder
+    temp_folder_path = os.path.join(os.path.dirname(__file__), "temp")
+
+    if not os.path.exists(temp_folder_path):
+        os.mkdir(temp_folder_path)
+
+    config_path = os.path.join(os.path.dirname(__file__), "..", "config.json")
+    if os.path.exists(config_path):
+        with open(config_path, "r") as fd:
+            content = json.load(fd)
+            onelab_path = content["onelab"]
+    elif os.path.isdir(os.path.join(os.path.dirname(__file__), "..", "..", "onelab")):
+        onelab_path = os.path.join(os.path.dirname(__file__), "..", "..", "onelab")
+    else:
+        print("FEMMT Simulations will not work without onelab path, which is not set yet. In order to \
+              run the tests please first specify a onelab path by creating a config.json in the tests folder.")
+        onelab_path = None
+
+    # Test
+    yield temp_folder_path, onelab_path
 
 def compare_result_logs(first_log_filepath: str, second_log_filepath: str, significant_digits: int = 6,
                         ignore_order: bool = True, number_format_notation: str = "f"):
@@ -105,31 +110,6 @@ def compare_thermal_result_logs(first_log_filepath: str, second_log_filepath: st
     # tried adding not existing keys in one of the dicts: results as expected in an error
     # changed values in very nested dict: results as expected in an error
     # So this command is valid to compare the dicts.
-
-
-@pytest.fixture
-def temp_folder():
-    """Fixture to create the temporary folder the results are stored."""
-    # Setup temp folder
-    temp_folder_path = os.path.join(os.path.dirname(__file__), "temp")
-
-    if not os.path.exists(temp_folder_path):
-        os.mkdir(temp_folder_path)
-
-    config_path = os.path.join(os.path.dirname(__file__), "..", "config.json")
-    if os.path.exists(config_path):
-        with open(config_path, "r") as fd:
-            content = json.load(fd)
-            onelab_path = content["onelab"]
-    elif os.path.isdir(os.path.join(os.path.dirname(__file__), "..", "..", "onelab")):
-        onelab_path = os.path.join(os.path.dirname(__file__), "..", "..", "onelab")
-    else:
-        print("FEMMT Simulations will not work without onelab path, which is not set yet. In order to \
-              run the tests please first specify a onelab path by creating a config.json in the tests folder.")
-        onelab_path = None
-
-    # Test
-    yield temp_folder_path, onelab_path
 
 
 @pytest.fixture
@@ -2848,261 +2828,6 @@ def test_load_files(temp_folder: pytest.fixture, fixture_inductor_core_material_
 # There is no result comparison
 ##############################
 
-
-def test_basic_example_inductor(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_inductor.basic_example_inductor(onelab_folder=onelab_folder,
-                                                         show_visual_outputs=False,
-                                                         is_test=True)
-
-
-def test_basic_example_transformer_interleaved(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_interleaved.basic_example_transformer_interleaved(onelab_folder=onelab_folder,
-                                                                                       show_visual_outputs=False,
-                                                                                       is_test=True)
-
-
-def test_basic_example_transformer(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer.basic_example_transformer(onelab_folder=onelab_folder, show_visual_outputs=False,
-                                                               is_test=True)
-
-
-def test_basic_example_transformer_three_winding(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_three_winding.basic_example_transformer_three_winding(onelab_folder=onelab_folder,
-                                                                                           show_visual_outputs=False,
-                                                                                           is_test=True)
-
-
-def test_basic_example_transformer_integrated(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_integrated.basic_example_transformer_integrated(onelab_folder=onelab_folder,
-                                                                                     show_visual_outputs=False,
-                                                                                     is_test=True)
-
-
-def test_basic_example_transformer_center_tapped(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_center_tapped.basic_example_transformer_center_tapped(onelab_folder=onelab_folder,
-                                                                                           show_visual_outputs=False,
-                                                                                           is_test=True)
-
-
-def test_basic_example_transformer_stacked(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_stacked.basic_example_transformer_stacked(onelab_folder=onelab_folder,
-                                                                               show_visual_outputs=False,
-                                                                               is_test=True)
-
-
-def test_basic_example_transformer_stacked_center_tapped(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_stacked_center_tapped.basic_example_transformer_stacked_center_tapped(
-        onelab_folder=onelab_folder, show_visual_outputs=False, is_test=True)
-
-
-def test_basic_example_inductor_foil_vertical(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_inductor_foil_vertical.basic_example_inductor_foil_vertical(onelab_folder=onelab_folder,
-                                                                                     show_visual_outputs=False,
-                                                                                     is_test=True)
-
-
-def test_basic_example_inductor_foil(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_inductor_foil.basic_example_inductor_foil(onelab_folder=onelab_folder,
-                                                                   show_visual_outputs=False,
-                                                                   is_test=True)
-
-
-def test_basic_example_transformer_n_winding(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_n_winding.basic_example_transformer_n_winding(onelab_folder=onelab_folder,
-                                                                                   show_visual_outputs=False,
-                                                                                   is_test=True)
-
-
-def test_basic_example_transformer_5_windings(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_5_windings.basic_example_transformer_5_windings(onelab_folder=onelab_folder,
-                                                                                     show_visual_outputs=False,
-                                                                                     is_test=True)
-
-
-def test_basic_example_transformer_6_windings(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_6_windings.basic_example_transformer_6_windings(onelab_folder=onelab_folder,
-                                                                                     show_visual_outputs=False,
-                                                                                     is_test=True)
-
-
-def test_basic_inductor_time_domain(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.experimental_inductor_time_domain.basic_example_inductor_time_domain(onelab_folder=onelab_folder,
-                                                                                        show_visual_outputs=False,
-                                                                                        is_test=True)
-
-
-def test_basic_transformer_time_domain(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.experimental_transformer_time_domain.basic_example_transformer_time_domain(onelab_folder=onelab_folder,
-                                                                                              show_visual_outputs=False,
-                                                                                              is_test=True)
-
-def test_basic_inductor_electrostatic(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_inductor_electrostatic.basic_example_inductor_electrostatic(onelab_folder=onelab_folder,
-                                                                                     show_visual_outputs=False,
-                                                                                     is_test=True)
-
-def test_basic_transformer_electrostatic(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.basic_transformer_electrostatic.basic_example_transformer_electrostatic(onelab_folder=onelab_folder,
-                                                                                           show_visual_outputs=False,
-                                                                                           is_test=True)
-
-
-def test_basic_transformer_3_windings_time_domain(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.experimental_transformer_three_winding_time_domain.basic_example_transformer_three_windings_time_domain(onelab_folder=onelab_folder,
-                                                                                                                           show_visual_outputs=False,
-                                                                                                                           is_test=True)
-
-
-def test_advanced_example_inductor_sweep(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.advanced_inductor_sweep.advanced_example_inductor_sweep(onelab_folder=onelab_folder,
-                                                                           show_visual_outputs=False,
-                                                                           is_test=True)
-
-
-def test_advanced_example_inductor_air_gap_sweep(temp_folder: pytest.fixture):
-    """
-    Integration test to the basic example file.
-    
-    :param temp_folder: temporary folder path and onelab filepath
-    :type temp_folder: pytest.fixture
-    """
-    temp_folder_path, onelab_folder = temp_folder
-    femmt.examples.advanced_inductor_air_gap_sweep.basic_example_sweep(onelab_folder=onelab_folder,
-                                                                       show_visual_outputs=False,
-                                                                       is_test=True)
 
 
 def test_transformer_component_study(temp_folder: pytest.fixture):
